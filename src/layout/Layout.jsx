52c0a9497bce5dcbe70ca9a3f96530e8555a7924
GabyRosas <fleuryrosas@gmail.com>
2024-06-19 16:02:07 +0200

    Router Ok

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,4 +1,15 @@
 import { Outlet} from "react-router-dom"
+
+const Layout = () => {
+    return (
+        <>
+            <main><Outlet /></main>
+            <footer>Footer</footer>
+        </>
+    )
+}
+
+export default Layoutimport { Outlet} from "react-router-dom"
 import Sidenav from "../components/navbar/Sidenav"
 
 const Layout = () => {
