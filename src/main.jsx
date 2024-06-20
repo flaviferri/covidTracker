4699cd896c04a748d560d02f34c3acb29513f1e1
Mercedes-Celedon <mercedes.celedon.95@gmail.com>
2024-06-20 00:57:22 -0600

    creating sidebar

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/Tracker1.jsx
@@ -0,0 +1,7 @@
+const Tracker1 = () =>{
+    return(
+        <><h1>tracker 1</h1></>
+    )
+};
+
+export default Tracker1
