8938aec6a1eb175be149401bd4d6b38d97aaf174
Mercedes-Celedon <mercedes.celedon.95@gmail.com>
2024-06-20 10:52:10 -0600

    adding style and variables scss

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,1 +1,13 @@
-hola
+import { Outlet} from "react-router-dom"
+import Sidenav from "../components/navbar/Sidenav"
+
+const Layout = () => {
+    return (
+        <>  
+            <Sidenav />
+            <main><Outlet /></main>
+        </>
+    )
+}
+
+export default Layout
