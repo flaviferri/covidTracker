1c511c7327d60ab582a2adc889b440aaa1e205f6
Lynn Poh <pohwlynn@gmail.com>
2024-06-21 13:54:54 +0200

    added Tracker2Comp. + column ids in table to allow  default sort

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,11 @@
 <<<<<<< HEAD
 import { Outlet} from "react-router-dom"
+import Sidenav from "../components/navbar/Sidenav"
 
 const Layout = () => {
     return (
         <>
+            <Sidenav/>
             <main><Outlet /></main>
             <footer>Footer</footer>
         </>
