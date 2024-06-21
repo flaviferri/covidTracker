5f06ca4fca9f8c53cc40c65aa40924acdf3f4b7d
Lynn Poh <pohwlynn@gmail.com>
2024-06-21 13:54:54 +0200

    added Tracker2Comp. + column ids in table to allow  default sort

--- src/components/Tracker2/Tr2Table.jsx
+++ src/components/Tracker2/Tr2Table.jsx
@@ -2,52 +2,62 @@ import React from 'react';
 
 const column = [
     {
+        id: "flag",
         name: "Flag",
         selector : row => row.countryInfo.flag,
         sortable: true,
         cell: row => <img src={row.countryInfo.flag} alt="flag" width="32" height="20" />
     }, 
     {
+        id: "country",
         name: "Country",
         selector : row => row.country,
         sortable: true
     }, 
     {
+        id: "cases",
         name: "Cases",
         selector : row => row.cases,
         sortable: true
     },
     {
+        id: "newCases",
         name: "New Cases",
         selector : row => row.todayCases,
         sortable: true
     },
     {
+        id: "deaths",
         name: "Deaths",
         selector : row => row.deaths,
         sortable: true
     },
     {
+        id: "newDeaths",
         name: "New Deaths",
         selector : row => row.todayDeaths,
         sortable: true
     },
     {
+        id: "recovered",
         name: "Recovered",
         selector : row => row.recovered,
         sortable: true
     },
     {
+        id: "active",
         name: "Active",
         selector : row => row.active,
         sortable: true
     },
     {
+        id: "critical",
         name: "Critical",
         selector : row => row.critical,
         sortable: true
     },
     {
+        id: "critical",
         name: "Tested",
         selector : row => row.tests,
         sortable: true
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,22 +1,12 @@
 import { Outlet} from "react-router-dom"
-
-const Layout = () => {
-    return (
-        <>
-            <main><Outlet /></main>
-            <footer>Footer</footer>
-        </>
-    )
-}
-
-export default Layoutimport { Outlet} from "react-router-dom"
 import Sidenav from "../components/navbar/Sidenav"
 
 const Layout = () => {
     return (
-        <>  
-            <Sidenav />
+        <>
+            <Sidenav/>
             <main><Outlet /></main>
+            <footer>Footer</footer>
         </>
     )
 }
