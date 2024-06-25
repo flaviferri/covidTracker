b44045b96d858fcebf20582dca9439d3c5e5fa6b
Carol <calonsogon6@gmail.com>
2024-06-25 15:48:41 +0200

    First commit HomePage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,26 @@
-/*faltan imports*/
-const Homepage = () =>{
-  return(
-      <><h1>Homepage</h1></>
-  )
+import React from 'react';
+import TextPart from '../../Seccions/FirstSection/textPart/TextPart';
+import ImagePart from '../../Seccions/FirstSection/imagePart/ImagePart';
+import Navbar from '../../Seccions/Header/navbar/Navbar';
+import SymptomSection from '../../Seccions/Symptom/symptomSection/SymptomSection';
+
+
+
+
+
+const Homepage = () => {
+  return (
+    <><div className='background'>
+          <Navbar />
+          <div className='content'>
+              <TextPart />
+              <ImagePart />
+          </div>
+      </div><div>
+              <SymptomSection />
+          </div></>
+      
+  );
 };
 
-export default Homepage
+export default Homepage;
