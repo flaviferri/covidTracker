9c408a39999b9d0bb7853f4611a8547cb2b5677f
Dpoetess <pohwlynn@gmail.com>
2024-06-25 13:50:06 +0200

    rebasing remote

--- src/services/useApi.jsx
+++ src/services/useApi.jsx
@@ -4,18 +4,31 @@ const useApi = (url) =>{
     const [data, setData] = useState(null)
     const [error, setError] = useState(null)
 
+<<<<<<< HEAD
     useEffect(()=>{ 
         const fetchData = async() =>{
+=======
+     useEffect(()=>{ 
+        const getData = async(url) =>{
+>>>>>>> 3e5f3c3 (initial api)
             try{
                 const response = await fetch(url);
                 if(!response.ok) {
                     throw new Error (`Error! status: ${response.status}`);
                 }
+<<<<<<< HEAD
                 const result = await response.json();
                 setData(result)
                 setError(null);
             } catch (error) {
                 setError(error.message);
+=======
+                const json_data = await response.json();
+                setData(json_data); // Actualiza el estados de los datos por los obtenidos
+    
+            }catch(error){
+              setError(error.message);
+>>>>>>> 3e5f3c3 (initial api)
                 console.error(`Error fetching data: ${error}`);
             } 
         };
