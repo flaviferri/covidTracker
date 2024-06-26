d696f92db72d7121d476caf54893f2b7c5454aa7
Lynn Poh <pohwlynn@gmail.com>
2024-06-26 12:16:45 +0200

    updated and rebased remote-local branch tracker2

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,48 +1,4 @@
-<<<<<<< HEAD
 import { Outlet} from "react-router-dom"
-
-const Layout = () => {
-    return (
-        <>
-            <main><Outlet /></main>
-            <footer>Footer</footer>
-        </>
-    )
-}
-
-export default Layoutimport { Outlet} from "react-router-dom"
-import Sidenav from "../components/navbar/Sidenav"
-import TrackerCovid from "../components/Tracker-Overall/TrackerCovid"
-import TrackerTop10 from "../components/Tracker-Top10/TrackerTop10"
-import "./layoutcss.scss";
-
-const Layout = () => {
-    return (
-        <>  
-            <Sidenav />
-            <div className="infoBox">
-            <TrackerCovid/>
-            <TrackerTop10/>
-            </div>
-            <main><Outlet /></main>
-        </>
-    )
-}
-
-export default Layoutimport { Outlet} from "react-router-dom"
-import Sidenav from "../components/navbar/Sidenav"
-
-const Layout = () => {
-    return (
-        <>
-            <Sidenav/>
-            <main><Outlet /></main>
-            <footer>Footer</footer>
-        </>
-    )
-}
-
-export default Layoutimport { Outlet} from "react-router-dom"
 import Sidenav from "../components/navbar/Sidenav"
 import TrackerCovid from "../components/Tracker-Overall/TrackerCovid"
 import TrackerTop10 from "../components/Tracker-Top10/TrackerTop10"
